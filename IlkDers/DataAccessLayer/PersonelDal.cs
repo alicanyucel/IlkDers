7c1da954e6d2ab94fcd43ef0c1bdcb477d650729
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-06 22:01:51 +0300

    Add project files.

---
+++ IlkDers/DataAccessLayer/PersonelDal.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace IlkDers.DataAccessLayer
+{
+    // burada crud operasyonlarını yapacağımız yer
+    public class PersonelDal
+    {
+        //serverin localdeki adresi                       //sunucu adresi.................... //database............. //wimdoew authanticationn 
+                                                                                                                     //şifre ve paralo girmeden
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-L6NJT48\\SQLEXPRESS;Initial Catalog=IlkDers;ıntegrated Security=True");
+               //geriye değer dondurmezse void olur
+        public   void ConnectionKontrol()
+        {
+                      //bağlantı kapalıysa
+            if(connection.State==ConnectionState.Closed)
+            {
+                connection.Open(); // bağlantıyı aç
+            }
+        }
+        // datatable aslında server side calısıyor en cok kullanılan yapı verileri dolduracak listeleyecek
+        public DataTable GetAll()
+        {
+            // BEST PARACTİTSE OLARAK
+            // ConnectionKontrol()
+            // VEYA
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            // SqlCommand tipinden command adı altında bir nesne oluşturdum ve ornekledim
+            // SqlCommand 2 tane parametre alır 1.sorgu 2. si bağlantı
+            SqlCommand command = new SqlCommand("Select * from Personel",connection);
+            // verileri okumak için SqlDataReadr var 
+            SqlDataReader reader = command.ExecuteReader(); // komutu çalıştırıyor okuma işlemi yapıldı
+            // Datatable dan dt adında nesne ürettik okduugmuz veriler dtnin içine atılacak
+            DataTable dt = new DataTable();
+            dt.Load(reader); // budada reader SqlDaraReaderin readaer nesnesinden gelen verileri dt dolduruyor
+            reader.Close(); // okuma işlemini sonlandırdık.
+            connection.Close(); // bağlantıyı kapattık
+            return dt; // datatable i donmemiz gerekiyor
+        }
+    }
+}
---
+++ IlkDers/Models/Personel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlkDers.Models
+{
+    // veri tabanındaki tablonun içeriğini burada entity ve property olarak karşıladık
+    public class Personel // bu enityudir
+    {
+        public int Id { get; set; } // propryilerd,r
+        public string Ad { get;set; }  // propryilerd,r
+        public string Soyad { get; set; }  // propryilerd,r
+        public string Brans { get;set; } // propryilerd,r
+
+    }
+}
